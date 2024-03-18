e64b2c4db1a5dbb8f90c62ba4fcfe34ee2c6c848
Jyothi <01fe22bca226@kletech.ac.in>
2024-03-18 17:35:11 +0530

    adding student information code

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    public static void main(String[] args) {
+        StudentCourses student01 = new StudentCourses("Jyoti", 226);
+
+        student01.semester01(93, 82);
+        student01.semester02(90, 85);
+        student01.semester03(92,94);
+
+        
+    }
+
+    
+}
